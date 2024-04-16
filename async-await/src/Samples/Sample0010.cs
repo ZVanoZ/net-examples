e3c46325b60e3b3751b74e74e2254b2f854b7f5f
Ivan Zahliada <edu.ZVanoZ@gmail.com>
2024-04-16 21:07:32 +0300

    async-await: 10 - улучшил вывод в консоль для улучшения читаемости

--- async-await/src/Samples/Sample0010.cs
+++ async-await/src/Samples/Sample0010.cs
@@ -1,4 +1,5 @@
 ﻿using common;
+using System;
 
 namespace Samples
 {
@@ -12,27 +13,40 @@ namespace Samples
 
             Common.WriteSeparator();
             Common.WriteSeparateString(GetType().Name);
-            
-            RunAsync().Wait();
-           
+
+            Console.WriteLine($"thread-id {Thread.CurrentThread.ManagedThreadId}:: Run - перед вызовом");
+            RunAsync().Wait(); // Запускаем асинхронный процесс и ждем
+            Console.WriteLine($"thread-id {Thread.CurrentThread.ManagedThreadId}:: Run - после вызова");
+
             Common.WriteSeparator();
         }
 
         static async Task RunAsync() {
-            await PrintAsync();   // вызов асинхронного метода
-            Console.WriteLine("Некоторые действия в методе Main");
+            Console.WriteLine($"thread-id {Thread.CurrentThread.ManagedThreadId}:: RunAsync BEG");
+
+            await PrintAsync("await");   // вызов асинхронного метода в синхронном режиме. Текущий поток ждет.
+            Console.WriteLine($"thread-id {Thread.CurrentThread.ManagedThreadId}:: RunAsync:: после await");
+
+            PrintAsync("Wait()").Wait(); // вызов асинхронного метода в асинхронном режиме с запуском ожидания через ".Wait()".
+            Console.WriteLine($"thread-id {Thread.CurrentThread.ManagedThreadId}:: RunAsync:: после Wait()");
+
+            Console.WriteLine($"thread-id {Thread.CurrentThread.ManagedThreadId}:: Некоторые действия в методе Main");
 
             void Print()
             {
+                Console.WriteLine($"thread-id {Thread.CurrentThread.ManagedThreadId}:: Print - начало");
+                Console.WriteLine($"thread-id {Thread.CurrentThread.ManagedThreadId}:: Print:: sleep...");
                 Thread.Sleep(3000);     // имитация продолжительной работы
-                Console.WriteLine("Hello METANIT.COM");
+                Console.WriteLine($"thread-id {Thread.CurrentThread.ManagedThreadId}:: Print - конец");
             }
             // определение асинхронного метода
-            async Task PrintAsync()
+            async Task PrintAsync(
+                string step
+                )
             {
-                Console.WriteLine("Начало метода PrintAsync"); // выполняется синхронно
-                await Task.Run(() => Print());                // выполняется асинхронно
-                Console.WriteLine("Конец метода PrintAsync");
+                Console.WriteLine($"thread-id {Thread.CurrentThread.ManagedThreadId}:: PrintAsync - Начало {step}"); // выполняется синхронно
+                await Task.Run(() => Print());                           // выполняется асинхронно
+                Console.WriteLine($"thread-id {Thread.CurrentThread.ManagedThreadId}:: PrintAsync - Конец {step}");
             }
         }
         
